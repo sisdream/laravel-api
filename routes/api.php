4589d83049c4a3f831c1d5282ca3f30b44910618
sisdream <sissipascale2@gmail.com>
2024-04-12 16:01:39 +0200

    feat: creato Api/ProjectController

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', function(){
+    dd('questa è una rotta');
+});
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
