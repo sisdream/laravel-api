40ad1b0b0f598196c416e9181799b161562f821d
sisdream <sissipascale2@gmail.com>
2024-04-13 20:48:57 +0200

    fix: modifica footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-dark text-white">
+<footer class="bg-dark text-white my-5">
   <div class="container py-4">
     &copy; {{ env('APP_AUTHOR', 'Alfonsina Pascale') }} {{ now()->year }}
   </div>
